978c7bf86019cbd76474202906e2db27ad8e7ca0
Bobby <h122656227@gmail.com>
2019-09-23 02:00:58 +0800

    add mysql module

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var http = require('http');
-var xml2js = require('xml2js');
-var xmlParser = new xml2js.Parser({explicitArray:false,ignoreAttrs:true})
-var fs = require('fs');
+const http = require('http');
+const xml2js = require('xml2js');
+const xmlParser = new xml2js.Parser({explicitArray:false,ignoreAttrs:true})
+const fs = require('fs');
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'qwer1234%^&*',
+    port: '3306',
+    database: 'politic',
+    multipleStatements: true
+})
+const addSql = 'INSERT INTO laslj(id,title,source,author,link,pubDate,description) VALUES(0,?,?,?,?,?,?)'
 
 http.get('http://www.people.com.cn/rss/politics.xml',function (data) {
-    var str = '';
+    let str = '';
     data.on("data",function (chunk) {
         str+=chunk;
     })
